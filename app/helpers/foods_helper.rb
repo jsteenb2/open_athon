a967001ee9bbe240d02473932695544143fa8a41
jsteenb2 <jonathansteenbergen@gmail.com>
2016-08-23 15:50:48 -0700

    added key to map and color scheme

--- app/helpers/foods_helper.rb
+++ app/helpers/foods_helper.rb
@@ -1,2 +1,12 @@
 module FoodsHelper
+
+  def color_pick(food)
+    if food.avg_score.to_i >= 92
+      return "primary"
+    elsif food.avg_score.to_i >= 86
+      return "warning"
+    else
+      return "danger"
+    end
+  end
 end
